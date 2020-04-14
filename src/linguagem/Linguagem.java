9bac373380df7e280f0f5f73165beb3322317662
Alanys Santos <alanyssantos161@gmail.com>
2020-04-13 22:55:09 -0300

    oi

--- src/linguagem/Linguagem.java
+++ src/linguagem/Linguagem.java
@@ -16,6 +16,8 @@ public class Linguagem {
      */
     public static void main(String[] args) {
         // TODO code application logic here
+        
+        System.out.println("Hello world!");
     }
     
 }
